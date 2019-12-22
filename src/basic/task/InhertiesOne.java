db7ebf0bc78781830dd12f3f42835586d30c6bb5
hasansadik <khondoker.hasan196@gmail.com>
2019-12-22 11:12:51 -0500

    inherit

---
+++ src/basic/task/InhertiesOne.java
@@ -0,0 +1,19 @@
+package basic.task;
+
+public class InhertiesOne {
+
+
+
+        static int Banana = 30;
+        static int Apple = 20;
+        static int Total = Banana + Apple;
+
+
+        public static void test() {
+
+            System.out.println(Total);
+
+        }
+
+
+    }
---
+++ src/basic/task/InhertiesTwo.java
@@ -0,0 +1,13 @@
+package basic.task;
+
+public class InhertiesTwo extends InhertiesOne{
+
+    public static void main(String[] args) {
+        test();
+    }
+
+
+
+}
+
+
