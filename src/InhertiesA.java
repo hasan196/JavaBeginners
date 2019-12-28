e2d7dc17540cde241b1487c2c95cabe06735a54c
hasansadik <khondoker.hasan196@gmail.com>
2019-12-28 09:54:50 -0500

    test class for InhertiesA

---
+++ src/InhertiesA.java
@@ -0,0 +1,15 @@
+import static java.lang.System.*;
+
+public class InhertiesA {
+
+    static int Banana=30;
+    static int Apple=40;
+    static int total=Banana+Apple;
+
+
+    public static void test() {
+        out.println(total);
+
+    }
+
+}
